b73ff28192c7632bd255ac9dc71a3ed2e21d21e9
Jan Stovicek <jan.stovicek@t-mobile.cz>
2021-05-12 17:38:26 +0200

    fix installation and compatibility issues

--- db/setup_db.sql
+++ db/setup_db.sql
@@ -1,3 +1,11 @@
--- this script should be used in order to help with an optimization of the used SQL queries
+-- this script should do the initial setup of the database
 
+CREATE DATABASE sparkscope;
+CREATE USER sparkscope_user WITH PASSWORD 'your_password';
+GRANT ALL PRIVILEGES ON DATABASE sparkscope TO sparkscope_user;
+ALTER DATABASE sparkscope OWNER sparkscope_user;
+
+
+
+-- optimize
 CREATE INDEX idx_stage__app_id ON stage (app_id);
--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ cp history_fetcher/config.ini.template history_fetcher/config.ini
 cp history_fetcher/logger.conf.template history_fetcher/logger.conf
 cp sparkscope_web/metrics/user_config.conf.template sparkscope_web/metrics/user_config.conf
 
-echo "export PYTHONPATH=$PYTHONPATH:`pwd`" >> venv/bin/activate
+echo 'export PYTHONPATH=$PYTHONPATH':`pwd` >> venv/bin/activate
